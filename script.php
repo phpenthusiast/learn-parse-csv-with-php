cb099da89e841ae19e1e04288b4cf73ab2f7c9f6
PHPenthusiast <support@phpenthusiast.com>
2018-05-10 19:48:40 +0300

    Create script.php

---
+++ script.php
@@ -0,0 +1,26 @@
+<?php
+
+$filename = 'cars.csv';
+
+// The nested array to hold all the arrays
+$the_big_array = []; 
+
+// Open the file for reading
+if (($h = fopen("{$filename}", "r")) !== FALSE) 
+{
+  // Each line in the file is converted into an individual array that we call $data
+  // The items of the array are comma separated
+  while (($data = fgetcsv($h, 1000, ",")) !== FALSE) 
+  {
+    // Each individual array is being pushed into the nested array
+    $the_big_array[] = $data;		
+  }
+
+  // Close the file
+  fclose($h);
+}
+
+// Display the code in a readable format
+echo "<pre>";
+var_dump($the_big_array);
+echo "</pre>";
